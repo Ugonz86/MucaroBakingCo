a6fbf93a69acae97745d5c2d3378dcf1c82fb7dc
Ugonz86 <ugonzalez86@gmail.com>
2023-04-11 18:22:10 -0700

    Added temp logos to Header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import { React, useState } from "react";
 import "./App.css";
-// import logo from "./logo.jpeg";
+import templogoowl from "./images/templogoowl.PNG";
+import tempTitle from "./images/tempTitle.PNG";
 import IconButton from "@mui/material/IconButton";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import FlutterDash from "@mui/icons-material/FlutterDash";
+// import FlutterDash from "@mui/icons-material/FlutterDash";
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -16,16 +17,17 @@ function Header() {
   return (
     <header className="Header">
       <Link to="/">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <FlutterDash className="flutter" />
+        <img src={templogoowl} className="App-logo" alt="logo" />
+        {/* <FlutterDash className="flutter" /> */}
       </Link>
 
       <div className="headerTitle">
-        <h1>MÚCARO</h1>
+        {/* <h1>MÚCARO</h1>
         <h4>
           <span style={{ color: "#d25876" }}>Baking</span>{" "}
           <span style={{ color: "#4a4949" }}>Co.</span>
-        </h4>
+        </h4> */}
+        <img src={tempTitle} className="App-title" alt="title" />
       </div>
       <nav>
         <div className="Menu">
@@ -50,38 +52,38 @@ function Header() {
         {/* <div className="container-fluid mt-3"> */}
         <div className="nav-small">
           {/* <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-md"> */}
-            {/* <div className="container-fluid p-2"> */}
-              {/* <a className="navbar-brand text-primary mr-0">Company Logo</a> */}
-              {/* <div className="form-inline ml-auto"> */}
-                {/* <div className="btn btn-primary" > */}
-                <IconButton
-                  aria-label="more"
-                  id="long-button"
-                  aria-controls={isOpen ? "long-menu" : undefined}
-                  aria-expanded={isOpen ? "true" : undefined}
-                  aria-haspopup="true"
-                  disableRipple
-                  onClick={ToggleSidebar}
-                  sx={{
-                    ml: 1,
-                    "&.MuiButtonBase-root:hover": {
-                      bgcolor: "transparent",
-                    },
-                  }}
-                  style={{
-                    color: "#4a4949",
-                    margin: "30px 0 30px",
-                    justifyContent: "center",
-                    alignItems: "center",
+          {/* <div className="container-fluid p-2"> */}
+          {/* <a className="navbar-brand text-primary mr-0">Company Logo</a> */}
+          {/* <div className="form-inline ml-auto"> */}
+          {/* <div className="btn btn-primary" > */}
+          <IconButton
+            aria-label="more"
+            id="long-button"
+            aria-controls={isOpen ? "long-menu" : undefined}
+            aria-expanded={isOpen ? "true" : undefined}
+            aria-haspopup="true"
+            disableRipple
+            onClick={ToggleSidebar}
+            sx={{
+              ml: 1,
+              "&.MuiButtonBase-root:hover": {
+                bgcolor: "transparent",
+              },
+            }}
+            style={{
+              color: "#4a4949",
+              margin: "30px 0 30px",
+              justifyContent: "center",
+              alignItems: "center",
 
-                    borderRadius: "5px",
-                  }}
-                >
-                  <MoreVertIcon />
-                </IconButton>
-                {/* </div> */}
-              {/* </div> */}
-            {/* </div> */}
+              borderRadius: "5px",
+            }}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          {/* </div> */}
+          {/* </div> */}
+          {/* </div> */}
           {/* </nav> */}
           <div className={`sidebar ${isOpen === true ? "active" : ""}`}>
             {/* <div className="sd-header">
