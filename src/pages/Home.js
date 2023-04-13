ad79e4673bb25d749b68c2bdabc0d84147b0cd5a
Ugonz86 <ugonzalez86@gmail.com>
2023-04-12 23:42:11 -0700

    Modified layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import "../App.css";
 // import InstaFeeds from "../components/InstaFeeds";
 import Carousel from "../components/Carousel";
+// import Header from "../Header";
 
 function Home() {
   return (
     <div className="Home">
+      {/* <Header /> */}
       <div className="homeContainer">
+
         <Carousel />
         <h1 style={{color: '#004c6d'}}>Coming Soon!</h1>
         {/* <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12} /> */}
