2cdef5706c07eb9f2c085e8cfe8e84b134d9bcad
Ugonz86 <ugonzalez86@gmail.com>
2023-04-12 18:12:57 -0700

    Fixed warnings

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import "../App.css";
 import SendIcon from "@mui/icons-material/Send";
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import "../App.css";
 // import InstaFeeds from "../components/InstaFeeds";
 import Carousel from "../components/Carousel";
