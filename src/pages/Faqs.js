f03662722863cbe1127f58470c0a7b1c25bb3556
Ugonz86 <ugonzalez86@gmail.com>
2023-04-25 17:13:58 -0700

    Replaced faqs popover with accordion

--- src/pages/Faqs.js
+++ src/pages/Faqs.js
@@ -1,68 +1,90 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import "../App.css";
+
+const faqQuestions = [
+  {
+    title: "How do I place an order?",
+    content: "...",
+  },
+  {
+    title: "I placed my order; now what?",
+    content:
+      "Thank you for your order! You will receive a confirmation email with the details of your order. Now the hard part begins; you will have to wait until pickup day. The location for the pickup varies, but it will be specified on the order form. The open hours will also vary but usually between 11:00 am and 3:00 pm. On the day of the Pop-Up event, drop by the location during the open hours of the Pop-Up, pick up your goodies, and enjoy!",
+  },
+  {
+    title: "Do you do deliveries?",
+    content:
+      "Unfortunately, not at this moment, but it could be a possibility once the micro-business grows.",
+  },
+  {
+    title: "Do you ship outside of Washington?",
+    content:
+      "Sadly, due to the type of food license, I am not allowed to make sales outside of Washington.",
+  },
+  {
+    title: "I placed an order but didn’t get the confirmation email",
+    content:
+      "Please allow ___ hours for your confirmation to arrive and check the spam/junk folder in your inbox. If you can’t locate it after ____ hours, please fill out the contact form or email aslyndiaz@gmail.com.",
+  },
+  {
+    title: "Can I walk in during the Pop-up?",
+    content:
+      "Of course! But preordering is highly recommended to guarantee you’ll be able to get your first choice of goodies. The items available to buy on the spot will be in limited quantities, and not all the items will be available to purchase on the day of the pop-up.",
+  },
+  {
+    title: "Do you take special orders?",
+    content:
+      "Yes! Please fill out the contact form or email aslyndiaz@gmail.com with the details.",
+  },
+  {
+    title: "Can I cancel my order?",
+    content:
+      "Cancellations will be accepted up to a week before the Pop-up event. If you need to cancel your order six to four days ahead, please fill out the contact form or email aslyndiaz@gmail.com. Those will be handled on a case-by-case basis. Changes will not be accepted within three days of the event.",
+  },
+];
+
+function Accordion({ faqQuestions }) {
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const handleClick = (index) => {
+    setActiveIndex(index === activeIndex ? -1 : index);
+  };
+  return (
+    <div className="accordionItem">
+      {faqQuestions.map((item, index) => (
+        <div className="accordionHeader" key={item.title}>
+          <h4
+            onClick={() => handleClick(index)}
+            style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}
+          >
+            {item.title}
+            <i>
+              <ArrowDropDownIcon className="accordionIcon" />
+            </i>
+          </h4>
+          {/* <h4 onClick={() =>handleClick(index)}><AddIcon />{item.title}</h4> */}
+          {index === activeIndex && (
+            <p className="accordionContent">{item.content}</p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function Faqs() {
   return (
     <div className="Faqs">
       <div className="faqsContainer">
         <h3 className="title">FAQ</h3>
-        <h4>How do I place an order?</h4>
-        <h4>I placed my order; now what?</h4>
-        <p>
-          Thank you for your order! You’ll receive a confirmation email with the
-          details of your order. Now the hard part begins; you'll have to wait
-          until pickup day. The location for the pickup varies, but it will be
-          specified on the order form. The open hours will also vary but usually
-          between 11:00 am and 3:00 pm. On the day of the Pop-Up event, drop by
-          the location during the open hours of the Pop-Up, pick up your
-          goodies, and enjoy!
-        </p>
-        <h4>Do you do deliveries?</h4>
-        <p>
-          Unfortunately, not at this moment, but it could be a possibility once
-          the micro-business grows.
-        </p>
-        <h4>Do you ship outside of Washington?</h4>
-        <p>
-          Sadly, due to the type of food license, I am not allowed to make sales
-          outside of Washington.
-        </p>
-        <h4>Do you ship within Washington?</h4>
-        <p>
-          Yes! This will be considered a special order; please fill out the
-          contact form or email hello@mucarobakingco.com instead of making an
-          order if interested.
-        </p>
-        <h4>I placed an order but didn’t get the confirmation email</h4>
-        <p>
-          Please allow ___ hours for your confirmation to arrive and check the
-          spam/junk folder in your inbox. If you can’t locate it after ____
-          hours, please fill out the contact form or email
-          aslyndiaz@gmail.com.
-        </p>
-        <h4>Can I walk in during the Pop-up?</h4>
-        <p>
-          Of course! But preordering is highly recommended to guarantee you’ll
-          be able to get your first choice of goodies. The items available to
-          buy on the spot will be in limited quantities, and not all the items
-          will be available to purchase on the day of the pop-up.
-        </p>
-        <h4>Do you take special orders?</h4>
-        <p>
-          Yes! Please fill out the contact form or email
-          aslyndiaz@gmail.com with the details.
-        </p>
-        <h4>Can I cancel my order?</h4>
-        <p>
-          Cancellations will be accepted up to a week before the Pop-up event.
-          If you need to cancel your order six to four days ahead, please fill
-          out the contact form or email aslyndiaz@gmail.com. Those will be
-          handled on a case-by-case basis. Changes will not be accepted within
-          three days of the event.
-        </p>
+        <Accordion faqQuestions={faqQuestions} />
         <h4>
-          For any other questions, please fill out the contact form or email
-          aslyndiaz@gmail.com
+          For any other questions, please fill out our{" "}
+          <Link to="/contact" className="faqLink">
+            Contact Form
+          </Link>{" "}
+          or email aslyndiaz@gmail.com
         </h4>
       </div>
     </div>
